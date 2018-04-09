41d319b516222129f7161d7f0f36975db9db1513
qxy <qxy65535@gmail.com>
2018-04-09 17:45:54 +0800

    fix

--- run.py
+++ run.py
@@ -3,6 +3,6 @@
 from app import app
 
 if (__name__ == '__main__'):
-    app.run('0.0.0.0', port=80, debug=False, threaded=True,
-            ssl_context=('/home/ubuntu/ssl/qsaltedfish/1_qsaltedfish.cn_bundle.crt',
-                         '/home/ubuntu/ssl/qsaltedfish/2_qsaltedfish.cn.key'))
+    app.run('0.0.0.0', port=8080, debug=False, threaded=True,
+            ssl_context=('D:/1work/ssl/qsaltedfish/Nginx/qsaltedfish.crt',
+                         'D:/1work/ssl/qsaltedfish/Nginx/qsaltedfish.key'))
